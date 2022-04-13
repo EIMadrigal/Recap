1b908f7434469c3e006b2ea00a17405c0e971e6c
EI Madrigal <renjingtaofc@163.com>
2022-04-13 17:27:55 +0800

    Update monotonous_stack.cpp

--- Templates/Advanced/monotonous_stack.cpp
+++ Templates/Advanced/monotonous_stack.cpp
@@ -1,29 +1,26 @@
-class MonotonousStack {
-public:
-    // 左边和右边第一个比nums[i]小(大)的位置 栈自底向上从小到大(从大到小)
-    // 数组中无重复值
-    vector<pair<int, int>> find(vector<int>& nums) {
-        vector<pair<int, int>> ans(nums.size());  // return index
-        for (int i = 0; i < nums.size(); ++i) {
-            while (!s.empty() && nums[i] < nums[s.top()]) {
-                int popIdx = s.top(); s.pop();
-                int leftIdx = s.empty() ? -1 : s.top();
-                ans[popIdx].first = leftIdx;
-                ans[popIdx].second = i;
-            }
-            s.push(i);
-        }
-        while (!s.empty()) {
-            int popIdx = s.top(); s.pop();
-            int leftIdx = s.empty() ? -1 : s.top();
-            ans[popIdx].first = leftIdx;
-            ans[popIdx].second = -1;
-        }
-        return ans;
-    } 
-private:
+// 左边和右边第一个比nums[i]小(大)的元素索引 栈自底向上从小到大(从大到小)
+
+// 数组中无重复值 first存左侧索引 second存右侧索引
+vector<pair<int, int>> monotonousStack(vector<int>& nums) {
     stack<int> s;  // store index
-};
+    vector<pair<int, int>> ans(nums.size());  // return index
+    
+    for (int i = 0; i < nums.size(); ++i) {
+        // 求smaller. 若需greater, 改为 nums[i] > nums[s.top()]
+        while (!s.empty() && nums[i] < nums[s.top()]) {
+            int idx = s.top(); s.pop();
+            ans[idx].first = s.empty() ? -1 : s.top();
+            ans[idx].second = i;
+        }
+        s.push(i);
+    }
+    while (!s.empty()) {
+        int popIdx = s.top(); s.pop();
+        ans[popIdx].first = s.empty() ? -1 : s.top();
+        ans[popIdx].second = -1;
+    }
+    return ans;
+} 
 
 // 有重复值, 主要为了处理左侧, 右侧其实可以沿用上面代码
 vector<vector<int>> monotonous_stack(vector<int>& nums) {
